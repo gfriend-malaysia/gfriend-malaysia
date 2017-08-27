c84259dc10fbd429bff4e0aa902a2388c082f9ed
hanjeahwan <hanjeahwan@gmail.com>
2017-08-27 22:49:45 +0800

    aa

--- sw.js
+++ sw.js
@@ -19,44 +19,36 @@ importScripts('workbox-sw.prod.v1.3.0.js');
  */
 const fileManifest = [
   {
-    "url": "/_nuxt/app.638a4e0aac4fdef15c30.js",
-    "revision": "b0aaddfd3fc6c04611a7f6f442d8fa78"
+    "url": "/_nuxt/app.9b99ab5993fff221c4af.js",
+    "revision": "b95ef58ae7499e64537686663d33902f"
   },
   {
-    "url": "/_nuxt/common.d465e797b584dd9cf770.js",
-    "revision": "5182d917ba7635d0a8eb187642377845"
+    "url": "/_nuxt/common.d2e17df68368c5ec6d24.js",
+    "revision": "30d3aa7343ba2f8f643d1d7aa8ae1824"
   },
   {
-    "url": "/_nuxt/layouts/default.92096d2f8dcaa33805bd.js",
-    "revision": "5bcbbd3c196d63d89304d31f0620e8ce"
+    "url": "/_nuxt/layouts/default.2c2cb0caa990940dfdb9.js",
+    "revision": "332dc8839f687b4f4d140391cb307bca"
   },
   {
-    "url": "/_nuxt/manifest.f9887927ac7433ca3065.js",
-    "revision": "179107197eebac4208ccf64b1b1f3abd"
+    "url": "/_nuxt/manifest.dea9350e52a74560b5a6.js",
+    "revision": "ff0ef6a661d46daa904284bbf0668240"
   },
   {
-    "url": "/_nuxt/pages/ats-home.38ce818ab85627786da1.js",
-    "revision": "29c16de03eb96e0f2e567039cd2fc36c"
+    "url": "/_nuxt/pages/ats.c9a56077213fbc7c8518.js",
+    "revision": "7ff159f6687f09ba51bd0b02e1f3920e"
   },
   {
-    "url": "/_nuxt/pages/ats-teamv1.ab895c5112e7760b734e.js",
-    "revision": "0a57a08bf32323f3a9b5e60f62868009"
+    "url": "/_nuxt/pages/career.6b387dbe47d92268eb67.js",
+    "revision": "61fc21fed18953717396e08d9fb64e4c"
   },
   {
-    "url": "/_nuxt/pages/ats.df22d98f95a3290c8579.js",
-    "revision": "bf8b7b8bdd1bbaeda6f333607e6aa007"
+    "url": "/_nuxt/pages/index.5bff94eab662cfc43437.js",
+    "revision": "141fac05a8a212b85eb4a46732303dc9"
   },
   {
-    "url": "/_nuxt/pages/career.b52384dac6ba538f2191.js",
-    "revision": "eebb9541c8b81c0d66339943d44dff1b"
-  },
-  {
-    "url": "/_nuxt/pages/index.4154fa47365b4325fe26.js",
-    "revision": "89af233b900b439ac9bba8d17a867f10"
-  },
-  {
-    "url": "/_nuxt/pages/job.2f8adc9d1fbcb7857cc1.js",
-    "revision": "b03066d4ebded38143bfa651f7222ac8"
+    "url": "/_nuxt/pages/job.7c0a8a479d08b9cb22f2.js",
+    "revision": "e26cb74705a3692a118dec72fb9a03bd"
   }
 ];
 
