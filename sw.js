fec7fd8e8083753c22e6e50dbb7ed171347f8e68
LAPTOP-JTRKUDVV\cychong <hanjeahwan@gmail.com>
2018-03-04 01:03:37 +0800

    x
    x

--- sw.js
+++ sw.js
@@ -8,24 +8,24 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.f9406842e5a0168e38e0.js",
-    "revision": "cd81167f947a7aead5a6924c723a5f7c"
+    "url": "/_nuxt/app.432e8471d2dc00d49ed0.js",
+    "revision": "ae7a7fb830caa4a5b59415e7b41f4753"
   },
   {
     "url": "/_nuxt/layouts_default.e44a1688b38f0f8002b7.js",
     "revision": "502393457dd6a13db4b01f83e6a73af1"
   },
   {
-    "url": "/_nuxt/manifest.3b6b6198eb59da933913.js",
-    "revision": "b85f9581da3acb0f5f4980c89d13497c"
+    "url": "/_nuxt/manifest.27f7282922921992285c.js",
+    "revision": "09459efb6a2e370a58ee48605a07afb4"
   },
   {
     "url": "/_nuxt/ons.40ac840e.js",
     "revision": "bdfc64563fadaafd6b988984058fefd9"
   },
   {
-    "url": "/_nuxt/pages_index.b6cf416b1fd7607e7a4e.js",
-    "revision": "e6780e23e1624cdff94b1c0c0ddc56c5"
+    "url": "/_nuxt/pages_index.979375be378670922e49.js",
+    "revision": "15504fefec7191724be426c4d446b8d2"
   },
   {
     "url": "/_nuxt/vendor.f94cc6b853312cac5e05.js",
