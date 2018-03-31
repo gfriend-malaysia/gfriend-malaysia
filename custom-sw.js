1b214d7630b4b8e1d47ca04fc6aba08b025df07d
LAPTOP-JTRKUDVV\cychong <hanjeahwan@gmail.com>
2018-03-31 17:33:10 +0800

    xx
    x

--- custom-sw.js
+++ custom-sw.js
@@ -1,1 +1,15 @@
-console.log('Custom service worker!')
+self.addEventListener('fetch', function(event) {
+	console.error('event', event)
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			// Cache hit - return response
+			if (response) {
+				console.error('response', response)
+
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
+});
