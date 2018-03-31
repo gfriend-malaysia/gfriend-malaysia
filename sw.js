6ae86372447b445d82bd04a92f2f7fbd8483a959
LAPTOP-JTRKUDVV\cychong <hanjeahwan@gmail.com>
2018-03-31 20:00:41 +0800

    x
    x

--- sw.js
+++ sw.js
@@ -42,13 +42,16 @@ workboxSW.precache([
 ])
 
 
-const matchCb = ({url, event}) => {
-  console.error('url',url)
-  console.error('event',event)
-  // return new RegExp('/_nuxt/.*')
+const matchCacheFirst = ({url, event}) => {
+  console.error('matchCacheFirst',url)
 };
+const matchNetworkFirst = ({url, event}) => {
+  console.error('matchNetworkFirst',url)
+};
+// workboxSW.router.registerRoute(new RegExp('/_nuxt/.*') , workboxSW.strategies.cacheFirst({}), 'GET')
 
-workboxSW.router.registerRoute(matchCb, workboxSW.strategies.cacheFirst({}), 'GET')
+// workboxSW.router.registerRoute(new RegExp('/.*'), workboxSW.strategies.networkFirst({}), 'GET')
 
-workboxSW.router.registerRoute(new RegExp('/.*'), workboxSW.strategies.networkFirst({}), 'GET')
+workboxSW.router.registerRoute(matchCacheFirst , workboxSW.strategies.cacheFirst({}), 'GET')
 
+workboxSW.router.registerRoute(matchNetworkFirst, workboxSW.strategies.networkFirst({}), 'GET')
