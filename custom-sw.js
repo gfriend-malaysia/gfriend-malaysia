fb4fd1dc25b88307945ecaf6238f5e4592cde9f6
LAPTOP-JTRKUDVV\cychong <hanjeahwan@gmail.com>
2018-03-31 17:46:31 +0800

    sad
    asd

--- custom-sw.js
+++ custom-sw.js
@@ -6,3 +6,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('Service worker activating...');
 });
+
+self.addEventListener('fetch', function(event) {
+  console.log('Fetching:', event.request.url);
+});
