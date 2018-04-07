d094e33f28aa435630a977d80cbdc1384677d903
LAPTOP-JTRKUDVV\cychong <hanjeahwan@gmail.com>
2018-04-08 00:09:06 +0800

    x
    x

--- sw.js
+++ sw.js
@@ -8,32 +8,32 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.68ef35412c868963586506f09de6e8cc.css",
-    "revision": "68ef35412c868963586506f09de6e8cc"
+    "url": "/_nuxt/app.c243825951b38556e442f5a2b69c96f9.css",
+    "revision": "c243825951b38556e442f5a2b69c96f9"
   },
   {
     "url": "/_nuxt/app.f0c14c2e9ed8061763eb.js",
     "revision": "c4091653d44e77a9a5b73b31b7a6cecd"
   },
   {
-    "url": "/_nuxt/layouts_default.9c6cec786eee0b94f252.js",
-    "revision": "2f8629f01eb9979f97020ea8c4b7efd0"
+    "url": "/_nuxt/layouts_default.5988098130d297c1ab69.js",
+    "revision": "b21e782f4a0409004ea41974cce7b5cf"
   },
   {
-    "url": "/_nuxt/manifest.b4dcc92e68d0a8721f8a.js",
-    "revision": "260f0b2dd3a0d1b62991f2cac7413d81"
+    "url": "/_nuxt/manifest.33cd8f848857ebc5220e.js",
+    "revision": "e252901b950c91d426e01d1604687c7f"
   },
   {
     "url": "/_nuxt/ons.40ac840e.js",
     "revision": "bdfc64563fadaafd6b988984058fefd9"
   },
   {
-    "url": "/_nuxt/pages_index.b11ddec7686c02b8f660.js",
-    "revision": "8ca11a908fe9d60bbca1a792f950ab2d"
+    "url": "/_nuxt/pages_index.3847e15098e444584fe4.js",
+    "revision": "d6d57129c36b98421cf9c02d510da32f"
   },
   {
-    "url": "/_nuxt/pages_test.dd81fb68124e6fdce529.js",
-    "revision": "e11ce98e82239ec04f9d04052c0dbb18"
+    "url": "/_nuxt/pages_test.ea5732184e256efb8d8a.js",
+    "revision": "b7ae0acf9f6ddb5476c105bed4260e93"
   },
   {
     "url": "/_nuxt/vendor.36cf00d878800eaa2041.js",
@@ -47,4 +47,6 @@ workboxSW.router.registerRoute(new RegExp('/_nuxt/.*'), workboxSW.strategies.cac
 workboxSW.router.registerRoute(new RegExp('/.*'), workboxSW.strategies.networkFirst({}), 'GET')
 
 workboxSW.router.registerRoute(new RegExp('https://jsonplaceholder.typicode.com/.*'), workboxSW.strategies.networkFirst({}), 'GET')
+
+workboxSW.router.registerRoute(new RegExp('https://dl.dropbox.com/s/.*'), workboxSW.strategies.networkFirst({}), 'GET')
 
